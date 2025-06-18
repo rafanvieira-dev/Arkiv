199a2fa135949ee066416178d6602fc4e07993f3
Rafael Nogueira <rafael.nogueira.5779@gmail.com>
2025-06-18 19:34:10 +0000

    Inserir uma barra de rolagem horizontal na lista de itens do acervo

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -18,6 +19,7 @@ const ScrollArea = React.forwardRef<
       {children}
     </ScrollAreaPrimitive.Viewport>
     <ScrollBar />
+    <ScrollBar orientation="horizontal" />
     <ScrollAreaPrimitive.Corner />
   </ScrollAreaPrimitive.Root>
 ))
