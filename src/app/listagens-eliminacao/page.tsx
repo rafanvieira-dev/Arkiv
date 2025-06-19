1cd7e6857f230179e631119a48b342303e8d69d7
Rafael Nogueira <rafael.nogueira.5779@gmail.com>
2025-06-19 19:58:53 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/listagens-eliminacao/page.tsx
+++ src/app/listagens-eliminacao/page.tsx
@@ -1,4 +1,6 @@
 
+"use client";
+
 import * as React from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
