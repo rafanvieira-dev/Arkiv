30d077f38dbf550aba8dd92d3aa944e2d4b6137f
Rafael Nogueira <rafael.nogueira.5779@gmail.com>
2025-07-16 04:02:27 +0000

    Eu me confundi na solicitação anterior. A barra vertical deverá aparecer

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -10,7 +10,6 @@ const Card = React.forwardRef<
     ref={ref}
     className={cn(
       "rounded-lg border bg-card text-card-foreground shadow-sm",
-      "flex flex-col",
       className
     )}
     {...props}
